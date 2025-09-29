736795df1816ac8073f911c95f315afeff8c97b1
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2025-09-29 12:51:41 -0500

    implemented prepend method in doubly linked list

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -44,6 +44,18 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
     
+    def prepend(self,value):
+        new_node = Node(value)
+        # case: list is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length -= 1
+    
     def print_list(self):
         temp = self.head
         while temp is not None:
@@ -51,5 +63,11 @@ class DoublyLinkedList:
             temp = temp.next
 
 
+my_list = DoublyLinkedList(3)
+my_list.append(4)
+my_list.append(5)
+my_list.append(6)
+my_list.prepend(2)
+my_list.print_list()
 
 
